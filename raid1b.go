10edd7730bb3e3719f2e8c232686e5e2c6fca949
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-19 20:51:56 +0600

    Update raid1b.go

--- raid1b.go
+++ raid1b.go
@@ -1,4 +1,4 @@
-package piscine
+package main
 
 import (
 	"github.com/01-edu/z01"
@@ -10,10 +10,22 @@ func Raid1b(x, y int) {
 	} else {
 		for i := 0; i < y; i++ {
 			for j := 0; j < x; j++ {
-				if i == 0 && j == 0 || i == y-1 && j == x-1 {
+				if i == 0 && j == 0 {
 					z01.PrintRune('/')
-				} else if i == 0 && j == x-1 || i == y-1 && j == 0 {
+				}else if i == y-1 && j == x-1{
+					if(x==1){
+						z01.PrintRune('/')
+					}else{
+						z01.PrintRune(92)
+					}
+				}else if  i == 0 && j == x-1  {
 					z01.PrintRune(92)
+				}else if i == y-1 && j == 0{
+					if (y==1){
+						z01.PrintRune('/')
+					}else{
+						z01.PrintRune(92)
+					}
 				} else if i == 0 || i == y-1 {
 					z01.PrintRune('*')
 				} else if j == 0 || j == x-1 {
@@ -27,4 +39,7 @@ func Raid1b(x, y int) {
 
 	}
 
+}
+func main(){
+	Raid1b(5,1)
 }
