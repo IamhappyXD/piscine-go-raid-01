56ee3192a0b8caee0f9c2888e0aeb76e9fc82bb9
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-19 19:49:03 +0600

    altyn's raid1a

--- raid1a.go
+++ raid1a.go
@@ -6,84 +6,67 @@ import (
 
 func Raid1a(x, y int) {
 
-	if x < 0 || y < 0 {
-		return
-	}
+	if x > 0 && y > 0{
+	
+		if x == 1 && y == 1 {
+			z01.PrintRune('o')
+			z01.PrintRune('\n')
+			return
+		} else if x == 1 {
+			z01.PrintRune('o')
+			for t := 1; t < y-1; t++ { //stolko raz skolko y - 2
+				if t <= 0 {
+					break
+				} else {
+					z01.PrintRune('\n')
+					z01.PrintRune('|')
 
-	if x == 0 || y == 0 {
-		z01.PrintRune('o')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		z01.PrintRune('o')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		z01.PrintRune('o')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		z01.PrintRune('o')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		return
-	}
-	if x == 1 && y == 1 {
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		return
-	} else if x == 1 {
-		z01.PrintRune('o')
-		for t := 1; t < y-1; t++ { //stolko raz skolko y - 2
-			if t <= 0 {
-				break
-			} else {
-				z01.PrintRune('\n')
+				}
+				if y == 1 {
+					z01.PrintRune('\n')
+					z01.PrintRune('o')
+					return
+				}
+			}
+			z01.PrintRune('\n')
+			z01.PrintRune('o')
+			z01.PrintRune('\n')
+
+		} else if y == 1 {
+			z01.PrintRune('o')
+			for i := 1; i < x-1; i++ {
+				z01.PrintRune('-') // 3 dash iz 5
+			}
+			z01.PrintRune('o')
+			z01.PrintRune('\n')
+			return
+		} else {
+			z01.PrintRune('o')
+			for i := 1; i < x-1; i++ {
+				z01.PrintRune('-') // 3 dash iz 5
+			}
+			z01.PrintRune('o')
+			z01.PrintRune('\n')
+
+			for t := 1; t < y-1; t++ { //stolko raz skolko y - 2
 				z01.PrintRune('|')
 
-			}
-			if y == 1 {
+				for j := 1; j < x-1; j++ {
+					z01.PrintRune(' ') // 3 pustoty iz 5
+				}
+				z01.PrintRune('|')
 				z01.PrintRune('\n')
-				z01.PrintRune('o')
-				return
+
 			}
-		}
-		z01.PrintRune('\n')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
 
-	} else if y == 1 {
-		z01.PrintRune('o')
-		for i := 1; i < x-1; i++ {
-			z01.PrintRune('-') // 3 dash iz 5
-		}
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		return
-	} else {
-		z01.PrintRune('o')
-		for i := 1; i < x-1; i++ {
-			z01.PrintRune('-') // 3 dash iz 5
-		}
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
+			z01.PrintRune('o')
 
-		for t := 1; t < y-1; t++ { //stolko raz skolko y - 2
-			z01.PrintRune('|')
-
-			for j := 1; j < x-1; j++ {
-				z01.PrintRune(' ') // 3 pustoty iz 5
+			for i := 1; i < x-1; i++ {
+				z01.PrintRune('-') // 3 dash iz 5
 			}
-			z01.PrintRune('|')
+			z01.PrintRune('o')
 			z01.PrintRune('\n')
 
 		}
-
-		z01.PrintRune('o')
-
-		for i := 1; i < x-1; i++ {
-			z01.PrintRune('-') // 3 dash iz 5
-		}
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-
 	}
-
 }
