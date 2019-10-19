d8d58e4ef74fb711f428498a5d32287ad82677c9
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-19 22:59:07 +0600

    ispravil

---
+++ raid1b.go
@@ -0,0 +1,46 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+//func main() {
+//	Raid1e(1, 1)
+//}
+
+func Raid1e(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	} else {
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				if i == 0 && j == 0 {
+					z01.PrintRune('/')
+				} else if i == y-1 && j == x-1 {
+					if y == 1 || x == 1 {
+						z01.PrintRune(92)
+					} else {
+						z01.PrintRune('/')
+					}
+				} else if i == 0 && j == x-1 {
+					z01.PrintRune(92)
+				} else if i == y-1 && j == 0 {
+					if x == 1 {
+						z01.PrintRune(92)
+					} else {
+						z01.PrintRune(92)
+					}
+				} else if i == 0 || i == y-1 {
+					z01.PrintRune('*')
+				} else if j == 0 || j == x-1 {
+					z01.PrintRune('*')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+
+	}
+
+}
