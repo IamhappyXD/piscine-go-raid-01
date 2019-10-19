a5fde2571e76d40bcd7ec33703346bd03d46ba20
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-19 22:46:19 +0600

    eee

--- raid1e.go
+++ raid1e.go
@@ -5,35 +5,35 @@ import (
 )
 
 //func main() {
-//	Raid1e(1, 1)
+//	Raid1e(1, 5)
 //}
 
-func Raid1b(x, y int) {
+func Raid1e(x, y int) {
 	if x <= 0 || y <= 0 {
 		return
 	} else {
 		for i := 0; i < y; i++ {
 			for j := 0; j < x; j++ {
 				if i == 0 && j == 0 {
-					z01.PrintRune('/')
+					z01.PrintRune('A')
 				} else if i == y-1 && j == x-1 {
 					if y == 1 || x == 1 {
-						z01.PrintRune(92)
+						z01.PrintRune('C')
 					} else {
-						z01.PrintRune('/')
+						z01.PrintRune('A')
 					}
 				} else if i == 0 && j == x-1 {
-					z01.PrintRune(92)
+					z01.PrintRune('C')
 				} else if i == y-1 && j == 0 {
 					if x == 1 {
-						z01.PrintRune(92)
+						z01.PrintRune('C')
 					} else {
-						z01.PrintRune(92)
+						z01.PrintRune('C')
 					}
 				} else if i == 0 || i == y-1 {
-					z01.PrintRune('*')
+					z01.PrintRune('B')
 				} else if j == 0 || j == x-1 {
-					z01.PrintRune('*')
+					z01.PrintRune('B')
 				} else {
 					z01.PrintRune(' ')
 				}
