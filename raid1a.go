81e1495de0f3decf6a8e20408ea4236eebc86d7d
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-19 19:06:11 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -9,6 +9,15 @@ func Raid1a(x, y int) {
 	if x < 0 || y < 0 {
 		return
 	}
+	if x == 0 || y == 0 {
+		z01.PrintRune('o')
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+		z01.PrintRune('o')
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+
+	}
 
 	z01.PrintRune('o')
 
